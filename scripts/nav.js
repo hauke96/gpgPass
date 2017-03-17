584535ec1de709ee0e5ed0744e23bdceb7807631
Hauke Stieler <mail@hauke-stieler.de>
2017-03-17 14:23:28 +0100

    Use theme and add interaction logic between toolbar and sidebar

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,7 +4,10 @@ document.body.addEventListener('click', function(event) {
     hideAllSectionsAndDeselectButtons()
 
     if (event.target.dataset.section) {
-        event.target.classList.add('is-selected')
+        //event.target.classList.add('is-selected')
+
+        selectItem('.nav-group-item', event.target.dataset.section, 'is-selected')
+        selectItem('.btn', event.target.dataset.section, 'active')
 
         // Display the current section
         const sectionId = event.target.dataset.section
@@ -12,14 +15,28 @@ document.body.addEventListener('click', function(event) {
     }
 })
 
+function selectItem(itemClass, sectionName, itemProperty) {
+    const items = document.querySelectorAll(itemClass)
+    Array.prototype.forEach.call(items, function(item) {
+        if (item.dataset.section === sectionName) {
+            item.classList.add(itemProperty)
+        }
+    })
+}
+
 function hideAllSectionsAndDeselectButtons() {
     const sections = document.querySelectorAll('.js-section.is-shown')
     Array.prototype.forEach.call(sections, function(section) {
         section.classList.remove('is-shown')
     })
 
-    const buttons = document.querySelectorAll('.nav-group-item.is-selected')
+    const buttons = document.querySelectorAll('.btn')
     Array.prototype.forEach.call(buttons, function(button) {
-        button.classList.remove('is-selected')
+        button.classList.remove('active')
+    })
+
+    const navItems = document.querySelectorAll('.nav-group-item.is-selected')
+    Array.prototype.forEach.call(navItems, function(item) {
+        item.classList.remove('is-selected')
     })
 }
